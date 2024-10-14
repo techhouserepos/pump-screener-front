9b6a61a600e1896a14fd01376b65a0917e75bf65
Aluisio Penna Ordones <aluisioordones1@gmail.com>
2024-10-14 20:09:08 -0300

    fix: secure prod test

--- middlewares/api_token.ts
+++ middlewares/api_token.ts
@@ -11,8 +11,11 @@ export default function apiTokenMiddleware(
 ) {
   try {
     const { api_token } = req.headers;
+    console.log("api token", api_token)
+    console.log("origin", process.env.ORIGIN)
     if (typeof api_token === "string" && api_token) {
       const decrypt = AES.decrypt(api_token, process.env.API_KEY || "secret").toString(enc.Utf8);
+      console.log("decrypt", decrypt)
       if (decrypt) {
         const parsed: ApiToken = JSON.parse(decrypt);
         return Date.now() < parsed.created + 5000 && parsed.location.startsWith(process.env.ORIGIN || "http://localhost:3000/");
